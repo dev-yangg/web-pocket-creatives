1a80658221adb9aef592de6dd631c3d0e29fdadc
yang <juliansalvador.dev@gmail.com>
2026-08-18 01:10:27 +0800

    feat(styles): implement z index for app header

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -4,7 +4,7 @@ import MenuButton from "./MenuButton";
 
 export default function AppHeader() {
   return (
-    <header className="w-app-header fixed top-4 left-1/2 -translate-x-1/2 flex items-center justify-between gap-x-2 shadow-[0_4px_16px_0] shadow-black/10 rounded-3xl px-8 py-2">
+    <header className="bg-white z-app-header w-app-header fixed top-4 left-1/2 -translate-x-1/2 flex items-center justify-between gap-x-2 shadow-[0_4px_16px_0] shadow-black/10 rounded-3xl px-8 py-2">
       <AppAnchor className="inline-block shrink-0" />
       <nav className="hidden md:block">
         <ul className="flex items-center justify-between">
