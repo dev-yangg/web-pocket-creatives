7569623d82acc841d60f64eb5c3ec1435d7bd6b8
yang <juliansalvador.dev@gmail.com>
2026-08-18 20:03:34 +0800

    fix(styles): adjust sizing of menu-btn

--- src/components/MenuButton.tsx
+++ src/components/MenuButton.tsx
@@ -1,6 +1,6 @@
 export default function MenuButton() {
   return (
-    <button className="w-10 h-6 flex md:hidden flex-col justify-between">
+    <button className="w-8 h-5 flex md:hidden flex-col justify-between">
       <div className="w-full h-1 bg-yellow" />
       <div className="w-full h-1 bg-yellow" />
       <div className="w-full h-1 bg-yellow" />
