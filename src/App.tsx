67063d3428f5d936e0acfdeb48343f2e72439464
yang <juliansalvador.dev@gmail.com>
2026-08-18 20:13:29 +0800

    fix(styles): move width constraint styling from main to individual sections instead

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import AppHeader from "./components/AppHeader";
 import Hero from "./sections/Hero";
 import PartnerShowcase from "./sections/PartnerShowcase";
+import VideoProdIntro from "./sections/VideoProdIntro";
 
 export default function App() {
   return (
     <>
       <AppHeader />
-      <main className="pt-28 w-app-boundary mx-auto">
+      <main className="pt-28">
         <Hero />
         <PartnerShowcase />
+        <VideoProdIntro />
       </main>
     </>
   );
