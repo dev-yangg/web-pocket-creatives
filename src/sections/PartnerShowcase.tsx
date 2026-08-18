bd2f8564da2696785eef0ccd6ce8b7ab98eb9951
yang <juliansalvador.dev@gmail.com>
2026-08-18 21:49:33 +0800

    fix(styles): refine text heading values @theme

--- src/sections/PartnerShowcase.tsx
+++ src/sections/PartnerShowcase.tsx
@@ -6,14 +6,14 @@ export default function PartnerShowcase() {
       <div className="content-boundary grid grid-cols-1 md:grid-cols-2 gap-12">
         <article className="text-grayblue text-pretty">
           <hgroup className="font-semibold flex flex-col gap-6">
-            <h2>
+            <h3>
               As you can see, we’ve{" "}
               <span className="text-yellow md:text-blue">partnered</span> with a
               lot of amazing people
-            </h2>
-            <h2 className="font-bold text-blue md:text-black">
+            </h3>
+            <h3 className="font-bold text-blue md:text-black">
               in many different Industries:
-            </h2>
+            </h3>
           </hgroup>
           <p className="mt-4">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente
