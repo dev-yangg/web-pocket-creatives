dacbf726d85d3b8bef555fd2c902921a755744b1
yang <juliansalvador.dev@gmail.com>
2026-08-19 22:45:32 +0800

    fix(styles): refine font-size

--- src/components/ContactCta.tsx
+++ src/components/ContactCta.tsx
@@ -9,10 +9,10 @@ export default function ContactCta({ className, label }: Props) {
   return (
     <button
       className={cn(
-        "order-3 md:order-2 @container justify-self-center bg-yellow md:bg-blue md:text-white font-bold leading-none w-24 md:w-14  aspect-square rounded-full rotate-10 drop-shadow-[3px_3px_3px] drop-shadow-grayblue",
+        "order-3 md:order-2 @container justify-self-center bg-yellow md:bg-blue md:text-white font-extrabold leading-none w-24 md:w-14  aspect-square rounded-full rotate-10 drop-shadow-[3px_3px_3px] drop-shadow-grayblue",
         className,
       )}>
-      <span className="text-[30cqw] inline-block">{label}</span>
+      <span className="text-[26cqw] inline-block">{label}</span>
     </button>
   );
 }
