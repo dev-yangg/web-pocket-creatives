7ccaaba65f4195952b975ede1a5cac3dc433b30f
yang <juliansalvador.dev@gmail.com>
2026-08-19 22:59:35 +0800

    feat(styles): add transition for color

--- src/components/ContactCta.tsx
+++ src/components/ContactCta.tsx
@@ -9,7 +9,7 @@ export default function ContactCta({ className, label }: Props) {
   return (
     <button
       className={cn(
-        "@container font-extrabold leading-none w-24 md:w-14  aspect-square rounded-full rotate-10 drop-shadow-[3px_3px_3px] drop-shadow-grayblue",
+        "@container font-extrabold leading-none w-24 md:w-14  aspect-square rounded-full rotate-10 drop-shadow-[3px_3px_3px] transition-colors duration-150 ease-swap drop-shadow-grayblue",
         className,
       )}>
       <span className="text-[26cqw] inline-block">{label}</span>
