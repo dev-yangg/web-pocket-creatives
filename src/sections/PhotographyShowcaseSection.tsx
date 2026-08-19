acfabaae635465c30d7bcb93f468e614418b690b
yang <juliansalvador.dev@gmail.com>
2026-08-19 21:21:54 +0800

    feat(styles): handle image behavior of photography showcase section

--- src/sections/PhotographyShowcaseSection.tsx
+++ src/sections/PhotographyShowcaseSection.tsx
@@ -23,33 +23,35 @@ export default function PhotographyShowcaseSection() {
           label="Photography"
         />
       </div>
-      <section className="grid grid-cols-1 md:grid-cols-2 content-boundary md:items-center gap-4">
-        <div className="rounded-md md:rounded-none overflow-clip md:order-2 h-full relative">
-          <img
-            src={photographDisplay}
-            alt="Beauty Preview"
-            loading="lazy"
-            className="md:absolute md:inset-0 w-full h-full object-cover object-center"
-          />
+      <section className="max-md:content-boundary md:bleed-grid">
+        <div className="grid grid-cols-1 md:grid-cols-3 md:items-center gap-4 md:gap-4 lg:gap-8 md:col-start-2 md:col-end-4">
+          <div className="md:col-start-2 md:col-end-4 rounded-md md:rounded-none overflow-clip md:order-2 h-full relative">
+            <img
+              src={photographDisplay}
+              alt="Beauty Preview"
+              loading="lazy"
+              className="md:absolute md:inset-0 w-full h-full object-cover object-center"
+            />
+          </div>
+          <ul className="flex flex-col">
+            {photographyCateg.map((categ) => (
+              <li
+                key={categ.name}
+                className="inline-block overflow-hidden custom-underline">
+                <a
+                  href={categ.path}
+                  className="inline-grid group py-1 font-bold text-heading-3">
+                  <span className="col-start-1 row-start-1 text-nowrap text-grayblue md:text-black group-hover:-translate-y-full transition-transform duration-150 ease-swap">
+                    {categ.name}
+                  </span>
+                  <span className="col-start-1 row-start-1 text-nowrap text-yellow translate-y-full group-hover:translate-y-0 transition-transform duration-150 ease-swap">
+                    View {categ.name}
+                  </span>
+                </a>
+              </li>
+            ))}
+          </ul>
         </div>
-        <ul className="flex flex-col">
-          {photographyCateg.map((categ) => (
-            <li
-              key={categ.name}
-              className="inline-block overflow-hidden custom-underline">
-              <a
-                href={categ.path}
-                className="inline-grid group py-1 font-bold text-heading-3">
-                <span className="col-start-1 row-start-1 text-nowrap text-grayblue md:text-black group-hover:-translate-y-full transition-transform duration-150 ease-swap">
-                  {categ.name}
-                </span>
-                <span className="col-start-1 row-start-1 text-nowrap text-yellow translate-y-full group-hover:translate-y-0 transition-transform duration-150 ease-swap">
-                  View {categ.name}
-                </span>
-              </a>
-            </li>
-          ))}
-        </ul>
       </section>
     </section>
   );
