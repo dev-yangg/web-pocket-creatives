c6b3a44ed82df33ecd685ca0929dce9c99fc5e28
yang <juliansalvador.dev@gmail.com>
2026-08-20 19:46:03 +0800

    feat(components): make ContactCta accept onClick props

--- src/components/ContactCta.tsx
+++ src/components/ContactCta.tsx
@@ -3,11 +3,13 @@ import { cn } from "../lib/utils";
 interface Props {
   className?: string;
   label: string;
+  onClick?: () => void;
 }
 
-export default function ContactCta({ className, label }: Props) {
+export default function ContactCta({ className, label, onClick }: Props) {
   return (
     <button
+      onClick={onClick}
       className={cn(
         "@container font-extrabold leading-none w-24 md:w-14  aspect-square rounded-full rotate-10 drop-shadow-[3px_3px_3px] transition-colors duration-150 ease-swap drop-shadow-grayblue",
         className,
