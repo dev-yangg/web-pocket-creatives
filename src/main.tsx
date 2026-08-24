9f2da4ea24d15e9c2ff67863f2f55a35b7c3a6e5
yang <juliansalvador.dev@gmail.com>
2026-08-24 18:21:47 +0800

    feat: add swiper css imports for carousel styling

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "@fontsource-variable/inter/wght.css";
 import "./styles/index.css";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/autoplay";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
