4db40221950f4fec66de773a0c286d7ee46bd091
yang <juliansalvador.dev@gmail.com>
2026-08-26 00:55:39 +0800

    feat: replace Homepage with react-router Outlet for nested routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
+import { Outlet } from "react-router";
 import AppHeader from "./components/AppHeader";
-import Homepage from "./pages/home/Homepage";
 
 export default function App() {
   return (
     <>
       <AppHeader />
       <main className="pt-28">
-        <Homepage />
+        <Outlet />
       </main>
     </>
   );
