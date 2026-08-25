b1e6f7824c1004ff28434a1735401753ab7aeba4
yang <juliansalvador.dev@gmail.com>
2026-08-25 19:09:02 +0800

    fix(styles): handle app header responsiveness

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -5,17 +5,17 @@ import MenuButton from "./MenuButton";
 
 export default function AppHeader() {
   return (
-    <header className="bg-white z-app-header w-app-header fixed top-4 left-1/2 -translate-x-1/2 flex items-center justify-between gap-x-2 shadow-[0_4px_16px_0] shadow-black/10 rounded-3xl px-8 py-2">
-      <AppAnchor className="inline-block shrink-0" />
-      <nav className="hidden md:block">
+    <header className="bg-white z-app-header w-app-header fixed top-4 left-1/2 -translate-x-1/2 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-[1fr_3fr_1fr] items-center gap-x-2 shadow-[0_4px_16px_0] shadow-black/10 rounded-3xl px-6 py-2">
+      <AppAnchor className="w-fit" />
+      <nav className="hidden min-[860px]:block">
         <ul className="flex items-center justify-between">
           <MainLinks />
         </ul>
       </nav>
-      <button className="shrink-0 w-8 aspect-square text-grayblue">
+      <button className="hidden sm:block justify-self-center min-[860px]:justify-self-end w-8 aspect-square text-grayblue">
         <FaWhatsapp className="fill-current w-full h-full" />
       </button>
-      <MenuButton />
+      <MenuButton className="justify-self-end flex flex-col justify-between min-[860px]:hidden" />
     </header>
   );
 }
--- src/components/MenuButton.tsx
+++ src/components/MenuButton.tsx
@@ -1,6 +1,8 @@
-export default function MenuButton() {
+import { cn } from "../lib/utils";
+
+export default function MenuButton({ className }: { className?: string }) {
   return (
-    <button className="w-8 h-5 flex md:hidden flex-col justify-between">
+    <button className={cn("w-8 h-5", className)}>
       <div className="w-full h-1 bg-yellow" />
       <div className="w-full h-1 bg-yellow" />
       <div className="w-full h-1 bg-yellow" />
