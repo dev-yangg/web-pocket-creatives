8c73295161ce06b29bb8a9e5043807355a745512
yang <juliansalvador.dev@gmail.com>
2026-08-27 01:52:13 +0800

    feat(pages): update sections

--- src/pages/about/AboutPage.tsx
+++ src/pages/about/AboutPage.tsx
@@ -1,11 +1,17 @@
+import Clients from "./sections/Clients";
+import DiscoveryNudge from "./sections/DiscoveryNudge";
 import Hero from "./sections/Hero";
 import StudioOverview from "./sections/StudioOverview";
+import ValueProposition from "./sections/ValueProposition";
 
 export default function AboutPage() {
   return (
     <>
       <Hero />
       <StudioOverview />
+      <ValueProposition />
+      <Clients />
+      <DiscoveryNudge />
     </>
   );
 }
