20f299312531c4a888966d781502861f81e8829d
yang <juliansalvador.dev@gmail.com>
2026-08-28 23:30:47 +0800

    fix(styles): move swiper/css into tailwind base layer to fix transition conflict

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "@fontsource-variable/inter/wght.css";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
 import "swiper/css/autoplay";
 import "./styles/index.css";
 import { RouterProvider } from "react-router";
