a48df45adf6ca3fca9c2ddef57651e458529e583
yang <juliansalvador.dev@gmail.com>
2026-09-02 22:58:24 +0800

    feat(components): use MainMenu component and its useHook

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import { Outlet } from "react-router";
 import AppHeader from "./components/AppHeader";
 import AppFooter from "./components/AppFooter";
+import MainMenu from "./components/MainMenu";
+import { useMenu } from "./hooks/useMenu";
 
 export default function App() {
+  const { isOpen, toggleMenu, closeMenu } = useMenu();
   return (
     <>
-      <AppHeader />
+      <AppHeader toggleMenu={toggleMenu} isOpen={isOpen} />
       <main className="pt-28">
         <Outlet />
       </main>
       <AppFooter />
+      <MainMenu isOpen={isOpen} closeMenu={closeMenu} />
     </>
   );
 }
--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -2,20 +2,31 @@ import { FaWhatsapp } from "react-icons/fa";
 import AppAnchor from "./AppAnchor";
 import MainLinks from "./MainLinks";
 import MenuButton from "./MenuButton";
+import type { UseMenuReturn } from "../hooks/useMenu";
 
-export default function AppHeader() {
+type AppHeaderProps = Pick<UseMenuReturn, "isOpen" | "toggleMenu">;
+
+export default function AppHeader({ isOpen, toggleMenu }: AppHeaderProps) {
   return (
-    <header className="bg-white z-app-header w-app-header fixed top-4 left-1/2 -translate-x-1/2 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-[1fr_3fr_1fr] items-center gap-x-2 shadow-[0_4px_16px_0] shadow-black/10 rounded-3xl px-6 py-2">
-      <AppAnchor className="w-fit" />
-      <nav className="hidden min-[860px]:block">
-        <ul className="flex items-center justify-between">
+    <header className="bg-white z-app-header w-app-header fixed top-4 left-1/2 -translate-x-1/2 shadow-[0_4px_16px_0] shadow-black/10 rounded-3xl px-6 py-2 grid grid-cols-[1fr_2fr_1fr] items-center">
+      <AppAnchor className="" />
+      <nav className="hidden nav-breakpoint:grid place-content-center">
+        <ul className="w-[min(800px,100%)] flex gap-x-4 items-center justify-between">
           <MainLinks />
         </ul>
       </nav>
-      <button className="hidden sm:block justify-self-center min-[860px]:justify-self-end w-8 aspect-square text-grayblue">
-        <FaWhatsapp className="fill-current w-full h-full" />
-      </button>
-      <MenuButton className="justify-self-end flex flex-col justify-between min-[860px]:hidden" />
+      <div className="grid place-content-center nav-breakpoint:place-content-end">
+        <button className=" w-8 aspect-square text-grayblue">
+          <FaWhatsapp className="fill-current hover:fill-[#25d366] transition-colors duration-150 ease-in-out w-full h-full " />
+        </button>
+      </div>
+      <div className="grid place-content-end">
+        <MenuButton
+          isOpen={isOpen}
+          onClick={toggleMenu}
+          className="flex flex-col nav-breakpoint:hidden justify-between"
+        />
+      </div>
     </header>
   );
 }
