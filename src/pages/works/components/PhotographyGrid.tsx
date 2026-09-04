e5ca6e1cb267d972d624aad4eb63bf53a7432671
yang <juliansalvador.dev@gmail.com>
2026-09-04 19:46:46 +0800

    fix(components): fix responsive layout of photography grid

--- src/pages/works/components/PhotographyGrid.tsx
+++ src/pages/works/components/PhotographyGrid.tsx
@@ -1,11 +1,39 @@
+import type { WorksGridSample } from "../data";
 import Skeleton from "./Skeleton";
 
-export default function PhotographyGrid() {
+export default function PhotographyGrid({
+  items,
+}: {
+  items: WorksGridSample[];
+}) {
+  const mockCells = Array.from({ length: 12 });
+  console.log({ "Photography Items": items });
   return (
-    <section className="grid grid-cols-2 md:grid-cols-4 gap-5">
-      {Array.from({ length: 12 }).map((_, index) => (
-        <Skeleton key={index} />
-      ))}
+    <section className="">
+      <div className="grid md:hidden grid-cols-2 gap-6">
+        {mockCells.map((_, index) => (
+          <Skeleton key={index} />
+        ))}
+      </div>
+      <div className="hidden md:grid md:grid-cols-3 gap-10">
+        {gridHelper(mockCells).map((groups, index) => (
+          <div key={index} className="grid grid-cols-2 gap-3">
+            {groups.map((_, index) => (
+              <Skeleton key={index} />
+            ))}
+          </div>
+        ))}
+      </div>
     </section>
   );
 }
+
+const gridHelper = <T,>(items: T[]): T[][] => {
+  const groups: T[][] = [[], [], []];
+  items.forEach((item, index) => {
+    const col = index % 6;
+    groups[Math.floor(col / 2)].push(item);
+  });
+
+  return groups;
+};
