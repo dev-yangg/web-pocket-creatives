2454199aea1cf18108247ae8e0e033c36927969c
yang <juliansalvador.dev@gmail.com>
2026-09-07 21:01:31 +0800

    refactor(components): isolate VideoCard into its own comp

---
+++ src/pages/works/components/VideoCard.tsx
@@ -0,0 +1,44 @@
+import { useRef, useState } from "react";
+import { cn } from "../../../lib/utils";
+import Skeleton from "./Skeleton";
+
+interface VideoCardProps {
+  label: string;
+  className?: string;
+  path: string;
+  alt: string;
+  onHover?: (el: HTMLVideoElement | null) => void;
+  onHoverEnd?: (el: HTMLVideoElement | null) => void;
+}
+
+export default function VideoCard({
+  label,
+  className,
+  path,
+  alt,
+  onHover,
+  onHoverEnd,
+}: VideoCardProps) {
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+  const [isVideoLoaded, setIsVideoLoaded] = useState(false);
+  return (
+    <figure className="flex flex-col relative isolate">
+      {!isVideoLoaded && <Skeleton className="absolute inset-0" />}
+      <video
+        ref={videoRef}
+        className={cn(className, {
+          "opacity-0 pointer-events-none": !isVideoLoaded,
+        })}
+        src={path}
+        loop
+        muted
+        playsInline
+        aria-label={alt}
+        onLoadedData={() => setIsVideoLoaded(true)}
+        onMouseEnter={() => onHover?.(videoRef.current)}
+        onMouseLeave={() => onHoverEnd?.(videoRef.current)}
+      />
+      {isVideoLoaded && <figcaption className="py-1">{label}</figcaption>}
+    </figure>
+  );
+}
--- src/pages/works/components/VideographyGrid.tsx
+++ src/pages/works/components/VideographyGrid.tsx
@@ -1,15 +1,5 @@
-import { useRef } from "react";
 import type { VideographySample } from "../data";
-import Skeleton from "./Skeleton";
-import { cn } from "../../../lib/utils";
-
-interface VideoCardProps {
-  className?: string;
-  path: string;
-  alt: string;
-  onHover?: (el: HTMLVideoElement | null) => void;
-  onHoverEnd?: (el: HTMLVideoElement | null) => void;
-}
+import VideoCard from "./VideoCard";
 
 export default function VideographyGrid({
   items,
@@ -27,44 +17,18 @@ export default function VideographyGrid({
   };
   return (
     <section className="grid grid-cols-1 md:grid-cols-4 gap-5">
-      {items.length < 1 &&
-        Array.from({ length: 12 }).map((_, index) => <Skeleton key={index} />)}
       {items.length > 0 &&
         items.map((item) => (
-          <figure key={item.id} className="flex flex-col">
-            <VideoCard
-              path={item.path}
-              alt={item.alt}
-              onHover={handleHover}
-              onHoverEnd={handleHoverEnd}
-              className="cursor-pointer"
-            />
-            <figcaption className="py-1">{item.label}</figcaption>
-          </figure>
+          <VideoCard
+            key={item.id}
+            label={item.label}
+            path={item.path}
+            alt={item.alt}
+            onHover={handleHover}
+            onHoverEnd={handleHoverEnd}
+            className="cursor-pointer"
+          />
         ))}
     </section>
-  );
-}
-
-function VideoCard({
-  className,
-  path,
-  alt,
-  onHover,
-  onHoverEnd,
-}: VideoCardProps) {
-  const videoRef = useRef<HTMLVideoElement | null>(null);
-  return (
-    <video
-      ref={videoRef}
-      className={cn(className)}
-      src={path}
-      loop
-      muted
-      playsInline
-      aria-label={alt}
-      onMouseEnter={() => onHover?.(videoRef.current)}
-      onMouseLeave={() => onHoverEnd?.(videoRef.current)}
-    />
   );
 }
