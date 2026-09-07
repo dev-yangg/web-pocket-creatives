01f20fee4806ad8e190b393e7e094dfa3c36dc73
yang <juliansalvador.dev@gmail.com>
2026-09-08 01:49:52 +0800

    feat(lib): implement useLockBodyScroll to prevent scrolling when floating elements are present

---
+++ src/hooks/useLockBodyScroll.ts
@@ -0,0 +1,12 @@
+import { useLayoutEffect } from "react";
+
+export function useLockBodyScroll() {
+  useLayoutEffect(() => {
+    const rootOverflow = document.body.style.overflow;
+    document.body.style.overflow = "hidden";
+
+    return () => {
+      document.body.style.overflow = rootOverflow;
+    };
+  }, []);
+}
