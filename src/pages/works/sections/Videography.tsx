d86be7220a28a70a79a9038d92c6a46df3de970e
yang <juliansalvador.dev@gmail.com>
2026-09-07 20:58:58 +0800

    feat(sections): only render videography section when items exist

--- src/pages/works/sections/Videography.tsx
+++ src/pages/works/sections/Videography.tsx
@@ -7,19 +7,21 @@ interface Props {
 
 export default function Videography({ category, items }: Props) {
   return (
-    <section className="py-14">
-      <div className="content-boundary flex flex-col gap-y-2 md:gap-y-4">
-        <header className="portfolio-gallery-header">
-          <h3 className="font-extrabold">
-            <span className="capitalize">{category}</span> Videography
-          </h3>
-          <small className="inline md:hidden">*Tap Video to play</small>
-          <small className="hidden md:inline">
-            *Click on Video to play in large view
-          </small>
-        </header>
-        <VideographyGrid items={items} />
-      </div>
-    </section>
+    items.length > 0 && (
+      <section className="py-14">
+        <div className="content-boundary flex flex-col gap-y-2 md:gap-y-4">
+          <header className="portfolio-gallery-header">
+            <h3 className="font-extrabold">
+              <span className="capitalize">{category}</span> Videography
+            </h3>
+            <small className="inline md:hidden">*Tap Video to play</small>
+            <small className="hidden md:inline">
+              *Click on Video to play in large view
+            </small>
+          </header>
+          <VideographyGrid items={items} />
+        </div>
+      </section>
+    )
   );
 }
