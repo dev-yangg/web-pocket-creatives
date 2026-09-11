cab31a10a1306038eb69930825aaef23a88d6eee
yang <juliansalvador.dev@gmail.com>
2026-09-12 00:48:46 +0800

    feat(pages): update contents of CTC page

--- src/pages/trainings/data.ts
+++ src/pages/trainings/data.ts
@@ -35,11 +35,84 @@ export const courseProcess = {
 };
 
 export const exampleCourses = {
+  headline: "Example courses",
   courses: [
-    "Those new to the industry might be interested in",
-    "Photographers adding video production to their offering",
-    "Video production professionals adding new services or sectors",
+    {
+      title: "Those new to the industry might be interested in",
+      items: [
+        "Camera setup and best practice",
+        "Lighting techniques",
+        "Getting started with professional video editing",
+      ],
+    },
+    {
+      title: "Photographers adding video production to their offering",
+      items: [
+        "How to take what you know in photography and apply it to moving imagery",
+        "Editing for video",
+      ],
+    },
+    {
+      title: "Video production professionals adding new services or sectors",
+      items: [
+        "You might work in food & drink but are keen to add the ability to shoot products",
+        "Tackling events",
+        "Working in the studio",
+        "Building confidence with recording sound",
+      ],
+    },
   ],
   ctaIcon: TbCurrencyPound,
   ctaLabel: "how much?",
+};
+
+export const btsShowcase = {
+  headline: "Want to see us at work?",
+  content: [
+    "If you hadn't already guessed, we love what we do, and we're proud of the video production projects that our team have delivered.",
+    "Have a look at some of these behind the scenes videos to see what it's like to be on set with us.",
+  ],
+  highlightedText: "behind the scenes",
+  videos: [
+    "/training/bts/bts-01.mp4",
+    "/training/bts/bts-02.mp4",
+    "/training/bts/bts-03.mp4",
+    "/training/bts/bts-04.mp4",
+  ],
+};
+
+export const clientsFeedback = {
+  headline: "What they say",
+  feedbacks: [
+    {
+      client: "Kitty Aldis, River Group",
+      text: "Fun, innovative and unflappable. Pocket Creatives are great to work with - whether it's finding the perfect lighting for a trifle to capturing GoPro footage on a whisk, they always approach every situation with a steady yet game-changing attitude. I see the whole team as trusted creative partners to make great content together.",
+      logo: "/training/clients/client-01-logo.png",
+    },
+    {
+      client: "Giang Cao, realme",
+      text: "Pocket Creatives is a superstar team that has consistently delivered above and beyond for the multiple projects they have worked on for us at realme. From product launch video production, video livesteaming and model photography, Steven and his talented team have met the brief and produced engaging and polished content. Starting with initial contact then meetings to parse out the project's requirements, planning, shooting days and editing, the process is always so seamless, professional and adept - even in the tightest of deadlines. The team approaches every brief with enthusiasm, bringing great ideas and flexibility when it comes to execution. Pocket Creatives is our first point of call when it comes to video production services and photography work and we highly recommend them",
+      logo: "/training/clients/client-02-logo.png",
+    },
+    {
+      client: "Alice Morgan, The Telegraph",
+      text: "The guys at Pocket Creatives were super knowledgeable and helpful - I definitely felt as though our video project was in safe hands! They were flexible and adaptable to what we needed and the content they produced for us was fantastic!",
+      logo: "/training/clients/client-03-logo.png",
+    },
+    {
+      client: "Holly Sud, DrPawPaw",
+      text: "The Pocket team were a pleasure to work with, not only were they super prepared, on time and professional but the quality of work they delivered in a short space of time was fab. I would recommend Pocket Creatives for their video production services, we couldn't have asked for a better experience and are thrilled with our short video.",
+      logo: "/training/clients/client-04-logo.png",
+    },
+    {
+      client: "Hayley Hall, Innovaderma/Skinny Tan",
+      text: "We were recommended Pocket Creatives by our PR agency, and have been thrilled with their professionalism, responsiveness, willingness to implement feedback and flexibility. Our internal challenges often mean we're a little last minute and work to short deadlines, but Pocket have exceeded our expectations and created a high standard of work with very little guidance. We've already commissioned additional activity and will be continuing to send work their way in the months and years to come.",
+      logo: "/training/clients/client-05-logo.png",
+    },
+    {
+      client: "Susie Willis, Romilly Wilde",
+      text: "Pocket Creatives were just what we had been looking for. Having a creative team that understand how the brand must communicate, with consistent and clean imagery is critical and PC really capture this. A lovely team to boot, who are obsessed with the detail and getting the most from the brief. Great team, multi-disciplined and talented bunch.",
+      logo: "/training/clients/client-06-logo.png",
+    },
+  ],
 };
