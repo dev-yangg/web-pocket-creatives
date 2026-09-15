78c672647c4224a441caeea9e482e5370a216a74
yang <juliansalvador.dev@gmail.com>
2026-09-15 21:13:11 +0800

    feat(sections): handle example courses collapsible element's transition for mobile breakpoint

--- src/pages/trainings/sections/ExampleCourses.tsx
+++ src/pages/trainings/sections/ExampleCourses.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { exampleCourses } from "../data";
 import { cn } from "../../../lib/utils";
+import { BsArrowLeft, BsArrowRight } from "react-icons/bs";
 
 export default function ExampleCourses() {
   const { headline, courses, ctaLabel, ctaIcon: Icon } = exampleCourses;
@@ -14,36 +15,49 @@ export default function ExampleCourses() {
         <h2 className="text-heading-1 capitalize font-extrabold text-center leading-none">
           {headline}
         </h2>
-        <ul className="flex gap-x-16 overflow-hidden">
+        <ul className="grid grid-cols-1 md:grid-cols-3 gap-8 @container">
           {courses.map((course, index) => {
             const target = openIndex === index;
 
             return (
               <li
                 key={course.title}
-                className={cn(
-                  " transition-[flex-grow_flex-shrink] duration-500 ease-in-out grid  min-w-0",
-                  target ? "flex-7 grid-cols-[300px_1fr]" : "flex-1",
-                )}>
+                className="grid grid-cols-1 grid-rows-1 overflow-hidden">
                 <h3
-                  className="bg-yellow text-heading-3 leading-none py-6 px-10 row-start-1 col-start-1 col-end-2"
-                  onClick={() => handleClick(index)}>
-                  {course.title}
+                  className={cn(
+                    "bg-yellow text-[clamp(1.75rem,3cqi,2rem)] leading-none py-16 md:py-0 px-6 lg:px-8 col-start-1 row-start-1 font-bold grid place-items-center transition-transform duration-300 ease-in-out text-balance",
+                    target ? "-translate-x-full" : "translate-x-0 z-10",
+                  )}>
+                  <span>
+                    {course.title}{" "}
+                    <button
+                      className="inline-block w-[1em] aspect-square align-[-0.25em] ml-8"
+                      onClick={() => handleClick(index)}>
+                      <BsArrowRight className="w-full h-full fill-current stroke-1 " />
+                    </button>
+                  </span>
                 </h3>
                 <div
                   className={cn(
-                    "bg-blue text-white py-6 px-10 row-start-1 col-start-1 col-end-2 -z-10",
-                    "col-start-2 col-end-3",
+                    "bg-blue text-white py-6 px-8 col-start-1 row-start-1 transition-transform duration-300 ease-in-out grid place-items-center",
+                    target
+                      ? "translate-x-0 z-10"
+                      : "translate-x-full pointer-events-none",
                   )}>
                   <ul className="flex flex-col gap-y-2">
                     {course.items.map((item) => (
                       <li
                         key={item}
-                        className="grid grid-cols-[auto_1fr] gap-x-2 items-baseline">
+                        className="grid grid-cols-[auto_1fr] gap-x-2 items-baseline text-[clamp(1.2rem,2cqi,2.25rem)]">
                         <span>•</span>
                         <span className="leading-tight">{item}</span>
                       </li>
                     ))}
+                    <button
+                      className="inline-block w-[clamp(1.75rem,3cqi,3rem)] aspect-square align-[-0.25em] ml-8 self-end mt-2"
+                      onClick={() => handleClick(index)}>
+                      <BsArrowLeft className="w-full h-full fill-current stroke-1" />
+                    </button>
                   </ul>
                 </div>
               </li>
