0ec7d8fbaf944e152e6fee81a20283ff8df4158b
yang <juliansalvador.dev@gmail.com>
2026-09-16 16:41:42 +0800

    feat(pages): add blog page seed data

---
+++ src/pages/blogs/Blogs.tsx
@@ -0,0 +1,9 @@
+export default function Blogs() {
+  return (
+    <section>
+      <div className="content-boundary">
+        <h1>Blogs Page</h1>
+      </div>
+    </section>
+  );
+}
---
+++ src/pages/blogs/data.ts
@@ -0,0 +1,62 @@
+export const blogs = [
+  {
+    title: "What is an Explainer Video? Benefits and Uses Explained ",
+    tags: ["beauty", "products", "video"],
+    image: "/blogs/blog-01.webp",
+  },
+  {
+    title: "Beauty Product Photography for Pony Revolution",
+    tags: ["beauty", "photography", "products", "video"],
+    image: "/blogs/blog-02.webp",
+  },
+  {
+    title: "Meeting Gouri Kubair at Holy Lama",
+    tags: ["beauty"],
+    image: "/blogs/blog-03.webp",
+  },
+  {
+    title: "Shooting Stop Motion Video",
+    tags: ["beauty", "video"],
+    image: "/blogs/blog-04.webp",
+  },
+  {
+    title: "Christmas Photography 2020",
+    tags: ["photography", "beauty", "jewellery", "products"],
+    image: "/blogs/blog-05.webp",
+  },
+  {
+    title: "6 Beauty Photography Tips",
+    tags: ["beauty"],
+    image: "/blogs/blog-06.webp",
+  },
+  {
+    title: "Model Behaviour",
+    tags: ["beauty", "people", "photography", "portraits"],
+    image: "/blogs/blog-07.webp",
+  },
+  {
+    title: "Created at home",
+    tags: ["photography", "beauty", "food", "food & drink", "products"],
+    image: "/blogs/blog-08.webp",
+  },
+  {
+    title: "Christmas Photography Content",
+    tags: ["photography", "beauty", "food", "food & drink", "watches"],
+    image: "/blogs/blog-09.webp",
+  },
+  {
+    title: "Fantastic Flat Lay",
+    tags: ["photography", "beauty", "food", "food & drink"],
+    image: "/blogs/blog-10.webp",
+  },
+  {
+    title: "Getting our nails into beauty",
+    tags: ["beauty", "video"],
+    image: "/blogs/blog-11.webp",
+  },
+  {
+    title: "Scrubs Up Well",
+    tags: ["crowdfunding", "beauty", "video"],
+    image: "/blogs/blog-12.webp",
+  },
+];
