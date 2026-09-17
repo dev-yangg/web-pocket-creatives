eaee48fd0a3ba335712a97d5bb0273fb19d75b0e
yang <juliansalvador.dev@gmail.com>
2026-09-17 22:30:37 +0800

    feat(components): add like and related news components

--- src/pages/blogs/components/BlogInnerTemplate.tsx
+++ src/pages/blogs/components/BlogInnerTemplate.tsx
@@ -2,6 +2,8 @@ import { SlArrowLeft } from "react-icons/sl";
 import { Link, useParams } from "react-router";
 import { blogs, type BlogBlock } from "../data";
 import EmptyStateHandler from "../../../components/EmptyStateHandler";
+import RelatedNews from "./RelatedNews";
+import LikeButton from "./LikeButton";
 
 interface ContentProps {
   block: BlogBlock;
@@ -14,63 +16,84 @@ export default function BlogInnerTemplate() {
   if (!blog) return <EmptyStateHandler />;
 
   return (
-    <article>
-      <div className="content-boundary">
-        <Link
-          to={"/blogs"}
-          className="text-small text-blue/75 hover:text-blue transition-colors duration-175 ease-swap inline-flex items-center gap-x-2 group">
-          <span className="inline-block h-[.7em] group-hover:-translate-x-0.5 transition-transform duration-175 ease-in-out">
-            <SlArrowLeft className="w-full h-full" />
-          </span>
-          <span className="group-hover:-translate-x-1 transition-transform duration-175 ease-in-out">
-            Go back to blogs
-          </span>
-        </Link>
-        <header className="flex flex-col gap-y-4 pt-3 pb-6 @container">
-          <h1 className="text-grayblue font-extrabold text-heading-2 leading-tight">
-            {blog.title}
-          </h1>
-          <div className="flex items-center flex-wrap gap-2 md:gap-4 text-blue text-[clamp(.85rem,1rem+0.5cqw,1.15rem)]">
-            <span>{blog.author}</span>
-            <span aria-hidden="true">•</span>
-            <span>{blog.createdAt}</span>
-            <ul className="md:ml-2 flex flex-wrap items-center rounded-lg overflow-clip bg-yellow font-extrabold capitalize text-black px-2 md:px-4 gap-x-3 leading-none py-2">
-              {blog.tags.map((tag, index) => {
-                const lastItem = index === blog.tags.length - 1;
+    <section>
+      <div className="content-boundary flex flex-col gap-y-8">
+        <article>
+          <Link
+            to={"/blogs"}
+            className="text-small text-blue/75 hover:text-blue transition-colors duration-175 ease-swap inline-flex items-center gap-x-2 group">
+            <span className="inline-block h-[.7em] group-hover:-translate-x-0.5 transition-transform duration-175 ease-in-out">
+              <SlArrowLeft className="w-full h-full" />
+            </span>
+            <span className="group-hover:-translate-x-1 transition-transform duration-175 ease-in-out">
+              Go back to blogs
+            </span>
+          </Link>
 
-                return (
-                  <li key={tag}>
-                    <span>
-                      {tag}
-                      {!lastItem && ","}
-                    </span>
-                  </li>
-                );
-              })}
-            </ul>
+          {/* Metadata */}
+          <header className="flex flex-col gap-y-4 pt-3 pb-6 @container">
+            <h1 className="text-grayblue font-extrabold text-heading-2 leading-tight">
+              {blog.title}
+            </h1>
+            <div className="flex items-center flex-wrap gap-2 md:gap-4 text-blue text-[clamp(.85rem,1rem+0.5cqw,1.15rem)]">
+              <span>{blog.author}</span>
+              <span aria-hidden="true">•</span>
+              <span>{blog.createdAt}</span>
+              <ul className="md:ml-2 flex flex-wrap items-center rounded-lg overflow-clip bg-yellow font-extrabold capitalize text-black px-2 md:px-4 gap-x-3 leading-none py-2">
+                {blog.tags.map((tag, index) => {
+                  const lastItem = index === blog.tags.length - 1;
+
+                  return (
+                    <li key={tag}>
+                      <span>
+                        {tag}
+                        {!lastItem && ","}
+                      </span>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          </header>
+
+          {/* Image */}
+          <div className="py-10">
+            <img
+              src={blog.image}
+              alt={blog.title}
+              className="w-full h-full object-cover aspect-5/2.5 object-center"
+            />
           </div>
-        </header>
-        <div className="py-10">
-          <img
-            src={blog.image}
-            alt={blog.title}
-            className="w-full h-full object-cover aspect-5/2.5 object-center"
-          />
-        </div>
-        {blog.content && blog.content.length > 0 ? (
-          <div className="grid grid-cols-1">
-            {blog.content.map((content, index) => (
-              <Content key={index} block={content} />
-            ))}
-          </div>
-        ) : (
-          <EmptyStateHandler
-            message="Content not available"
-            className="text-heading-4 py-14"
-          />
+
+          {/* Content */}
+          {blog.content && blog.content.length > 0 ? (
+            <div className="grid grid-cols-1">
+              {blog.content.map((content, index) => (
+                <Content key={index} block={content} />
+              ))}
+            </div>
+          ) : (
+            <EmptyStateHandler
+              message="Content not available"
+              className="text-heading-4 pt-8 pb-18"
+            />
+          )}
+
+          {/* Like button */}
+          {blog.content && blog.content.length > 0 && (
+            <div className="flex flex-col xs:flex-row xs:items-center gap-x-4 gap-y-8">
+              <LikeButton likeCount={blog.likeCount} />
+              <div className="h-full w-full grow relative after:absolute after:inset-x-0 after:top-1/2 after:-translate-y-1/2 after:bg-grayblue after:h-0.5"></div>
+            </div>
+          )}
+        </article>
+
+        {/* Related News Sec */}
+        {blog.relatedIds && blog.relatedIds.length > 0 && (
+          <RelatedNews relatedIds={blog.relatedIds} />
         )}
       </div>
-    </article>
+    </section>
   );
 }
 
