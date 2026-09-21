e226396e3336538848a5ebcd9fc78c8110c908b7
yang <juliansalvador.dev@gmail.com>
2026-09-22 00:56:47 +0800

    feat(data): add image paths

--- src/pages/services/data.ts
+++ src/pages/services/data.ts
@@ -24,8 +24,9 @@ interface ProcessStepImage {
 
 interface ProcessStep {
   title: string;
+  subtitle: string;
   description: string;
-  images: ProcessStepImage[];
+  image: ProcessStepImage;
 }
 
 export interface ServicesProcess {
@@ -38,7 +39,7 @@ export const servicesIntro1: ServicesIntro = {
   headline: "Aaaand Action:",
   headlineHighlight: "Video Production",
   content:
-    "Lorem ipsum dolor sit amet consectetur. Nulla purus rhoncus at mattis. Et ac vitae ornare volutpat. Mollis sem scelerisque dictum nunc iaculis vivamus donec. Molestie sed mattis aenean sit arcu ipsum amet vulputate tellus. Blandit pellentesque magna egestas eget rhoncus tincidunt. In ultrices velit et velit morbi vitae dolor fames. Rhoncus.",
+    "Lorem ipsum dolor sit amet consectetur. Morbi mi elit vulputate aliquet amet. Consectetur imperdiet ac tortor urna. Amet convallis cras amet tortor massa odio consectetur. Nunc egestas euismod odio ullamcorper. Risus pharetra massa quam tincidunt odio senectus nunc pulvinar nisl. Commodo mattis quis mauris libero aliquet nunc quis aliquam. Ultricies. Mollis sem scelerisque dictum nunc iaculis vivamus donec. Molestie sed mattis aenean sit arcu ipsum amet vulputate tellus. Blandit pellentesque magna egestas eget rhoncus tincidunt. In ultrices velit et velit morbi vitae dolor fames. Rhoncus.",
   ctas: {
     ctaLabel: "Go to...",
     links: [
@@ -52,7 +53,7 @@ export const servicesIntro2: ServicesIntro = {
   headline: "Pixel perfection:",
   headlineHighlight: "Photography",
   content:
-    "Lorem ipsum dolor sit amet consectetur. Nulla purus rhoncus at mattis. Et ac vitae ornare volutpat. Mollis sem scelerisque dictum nunc iaculis vivamus donec. Molestie sed mattis aenean sit arcu ipsum amet vulputate tellus. Blandit pellentesque magna egestas eget rhoncus tincidunt. In ultrices velit et velit morbi vitae dolor fames. Rhoncus.",
+    "Lorem ipsum dolor sit amet consectetur. Morbi mi elit vulputate aliquet amet. Consectetur imperdiet ac tortor urna. Amet convallis cras amet tortor massa odio consectetur. Nunc egestas euismod odio ullamcorper. Risus pharetra massa quam tincidunt odio senectus nunc pulvinar nisl. Commodo mattis quis mauris libero aliquet nunc quis aliquam. Ultricies. Mollis sem scelerisque dictum nunc iaculis vivamus donec. Molestie sed mattis aenean sit arcu ipsum amet vulputate tellus. Blandit pellentesque magna egestas eget rhoncus tincidunt. In ultrices velit et velit morbi vitae dolor fames. Rhoncus.",
   ctas: {
     ctaLabel: "Go to...",
     links: [
@@ -66,7 +67,7 @@ export const servicesIntro3: ServicesIntro = {
   headline: "We've got it covered:",
   headlineHighlight: "The Whole Package",
   content:
-    "Lorem ipsum dolor sit amet consectetur. Nulla purus rhoncus at mattis. Et ac vitae ornare volutpat. Mollis sem scelerisque dictum nunc iaculis vivamus donec. Molestie sed mattis aenean sit arcu ipsum amet vulputate tellus. Blandit pellentesque magna egestas eget rhoncus tincidunt. In ultrices velit et velit morbi vitae dolor fames. Rhoncus.",
+    "Lorem ipsum dolor sit amet consectetur. Morbi mi elit vulputate aliquet amet. Consectetur imperdiet ac tortor urna. Amet convallis cras amet tortor massa odio consectetur. Nunc egestas euismod odio ullamcorper. Risus pharetra massa quam tincidunt odio senectus nunc pulvinar nisl. Commodo mattis quis mauris libero aliquet nunc quis aliquam. Ultricies. Mollis sem scelerisque dictum nunc iaculis vivamus donec. Molestie sed mattis aenean sit arcu ipsum amet vulputate tellus. Blandit pellentesque magna egestas eget rhoncus tincidunt. In ultrices velit et velit morbi vitae dolor fames. Rhoncus.",
   ctas: {
     ctaLabel: "Go to...",
     links: [
@@ -82,28 +83,33 @@ export const servicesProcess1: ServicesProcess = {
   steps: [
     {
       title: "Pre-production",
+      subtitle: "Meeting & Planning",
       description:
-        "Lorem ipsum dolor sit amet consectetur. Nulla purus rhoncus at mattis. Et ac vitae ornare volutpat.",
-      images: [
-        { src: "", alt: "Team meeting to plan a video production project" },
-      ],
+        "Lorem ipsum dolor sit amet consectetur. Morbi mi elit vulputate aliquet amet. Consectetur imperdiet ac tortor urna. Amet convallis cras amet tortor massa odio consectetur. Nunc egestas euismod odio ullamcorper. Risus pharetra massa quam tincidunt odio senectus nunc pulvinar nisl. Commodo mattis quis mauris libero aliquet nunc quis aliquam. Ultricies.",
+      image: {
+        src: "/services/process-01/service-process-1-pre-pod.webp",
+        alt: "Team chatting around a cafe table while planning a video project",
+      },
     },
     {
       title: "Production",
+      subtitle: "Lights, Camera, Action",
       description:
-        "Lorem ipsum dolor sit amet consectetur. Nulla purus rhoncus at mattis. Et ac vitae ornare volutpat.",
-      images: [
-        { src: "", alt: "Close-up of a professional video camera on set" },
-      ],
+        "Lorem ipsum dolor sit amet consectetur. Morbi mi elit vulputate aliquet amet. Consectetur imperdiet ac tortor urna. Amet convallis cras amet tortor massa odio consectetur. Nunc egestas euismod odio ullamcorper. Risus pharetra massa quam tincidunt odio senectus nunc pulvinar nisl. Commodo mattis quis mauris libero aliquet nunc quis aliquam. Ultricies.",
+      image: {
+        src: "/services/process-01/service-process-1-prod.webp",
+        alt: "Close-up of a professional video camera on set",
+      },
     },
     {
       title: "Post-production",
+      subtitle: "Review & Edit",
       description:
-        "Lorem ipsum dolor sit amet consectetur. Nulla purus rhoncus at mattis. Et ac vitae ornare volutpat.",
-      images: [
-        { src: "", alt: "Editor reviewing footage on multiple monitors" },
-        { src: "", alt: "Video editing timeline with clips on screen" },
-      ],
+        "Lorem ipsum dolor sit amet consectetur. Morbi mi elit vulputate aliquet amet. Consectetur imperdiet ac tortor urna. Amet convallis cras amet tortor massa odio consectetur. Nunc egestas euismod odio ullamcorper. Risus pharetra massa quam tincidunt odio senectus nunc pulvinar nisl. Commodo mattis quis mauris libero aliquet nunc quis aliquam. Ultricies.",
+      image: {
+        src: "/services/process-01/service-process-1-post-prod.webp",
+        alt: "Editor reviewing footage on multiple monitors",
+      },
     },
   ],
 };
@@ -114,28 +120,33 @@ export const servicesProcess2: ServicesProcess = {
   steps: [
     {
       title: "Pre-production",
+      subtitle: "Meeting & Planning",
       description:
-        "Lorem ipsum dolor sit amet consectetur. Nulla purus rhoncus at mattis. Et ac vitae ornare volutpat.",
-      images: [
-        { src: "", alt: "Team meeting to plan a video production project" },
-      ],
+        "Lorem ipsum dolor sit amet consectetur. Morbi mi elit vulputate aliquet amet. Consectetur imperdiet ac tortor urna. Amet convallis cras amet tortor massa odio consectetur. Nunc egestas euismod odio ullamcorper. Risus pharetra massa quam tincidunt odio senectus nunc pulvinar nisl. Commodo mattis quis mauris libero aliquet nunc quis aliquam. Ultricies.",
+      image: {
+        src: "/services/process-02/service-process-2-pre-prod.webp",
+        alt: "Three women discussing a shoot around a laptop, one holding a camera",
+      },
     },
     {
       title: "Production",
+      subtitle: "Lights, Camera, Action",
       description:
-        "Lorem ipsum dolor sit amet consectetur. Nulla purus rhoncus at mattis. Et ac vitae ornare volutpat.",
-      images: [
-        { src: "", alt: "Close-up of a professional video camera on set" },
-      ],
+        "Lorem ipsum dolor sit amet consectetur. Morbi mi elit vulputate aliquet amet. Consectetur imperdiet ac tortor urna. Amet convallis cras amet tortor massa odio consectetur. Nunc egestas euismod odio ullamcorper. Risus pharetra massa quam tincidunt odio senectus nunc pulvinar nisl. Commodo mattis quis mauris libero aliquet nunc quis aliquam. Ultricies.",
+      image: {
+        src: "/services/process-02/service-process-2-prod.webp",
+        alt: "Photographer shooting a smiling woman applying a serum dropper",
+      },
     },
     {
       title: "Post-production",
+      subtitle: "Review & Edit",
       description:
-        "Lorem ipsum dolor sit amet consectetur. Nulla purus rhoncus at mattis. Et ac vitae ornare volutpat.",
-      images: [
-        { src: "", alt: "Editor reviewing footage on multiple monitors" },
-        { src: "", alt: "Video editing timeline with clips on screen" },
-      ],
+        "Lorem ipsum dolor sit amet consectetur. Morbi mi elit vulputate aliquet amet. Consectetur imperdiet ac tortor urna. Amet convallis cras amet tortor massa odio consectetur. Nunc egestas euismod odio ullamcorper. Risus pharetra massa quam tincidunt odio senectus nunc pulvinar nisl. Commodo mattis quis mauris libero aliquet nunc quis aliquam. Ultricies.",
+      image: {
+        src: "/services/process-02/service-process-2-post-prod.webp",
+        alt: "Editor in a cap reviewing footage on a monitor",
+      },
     },
   ],
 };
@@ -154,7 +165,7 @@ export const servicesBreakdown1 = {
       description:
         "Nunc id ridiculus mattis ullamcorper in id accumsan eu habitant. Mollis duis eu tellus aliquam viverra. Amet feugiat et habitasse mauris massa iaculis.",
       image: {
-        src: "",
+        src: "/services/breakdown/planning-image.webp",
         alt: "Person on a phone call reviewing a tablet while planning",
       },
     },
@@ -169,7 +180,7 @@ export const servicesBreakdown1 = {
       description:
         "Nunc id ridiculus mattis ullamcorper in id accumsan eu habitant. Mollis duis eu tellus aliquam viverra. Amet feugiat et habitasse mauris massa iaculis.",
       image: {
-        src: "",
+        src: "/services/breakdown/talent-location-image.webp",
         alt: "Model in patterned outfit posing on a cliffside location",
       },
     },
@@ -184,7 +195,7 @@ export const servicesBreakdown1 = {
       description:
         "Nunc id ridiculus mattis ullamcorper in id accumsan eu habitant. Mollis duis eu tellus aliquam viverra. Amet feugiat et habitasse mauris massa iaculis.",
       image: {
-        src: "",
+        src: "/services/breakdown/editing-delivery-image.webp",
         alt: "Monitor displaying a photo editing grid at a desk",
       },
     },
