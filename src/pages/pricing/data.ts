72bc212615787b6ac7d2752336c5511f244e99c0
yang <juliansalvador.dev@gmail.com>
2026-09-23 21:53:53 +0800

    feat(pages): add Svg icons to pricing overview data

--- src/pages/pricing/data.ts
+++ src/pages/pricing/data.ts
@@ -1,3 +1,5 @@
+import EuroSvg01 from "../../components/EuroSvg01";
+import EuroSvg02 from "../../components/EuroSvg02";
 import { baseContactFormFields } from "../../data/globals";
 
 export const heroData = {
@@ -17,15 +19,15 @@ export const pricingOverviewData = {
     "No matter how large or small your project",
     "Oh, and no budget overruns!",
   ],
-  cards: [
+  infoBlocks: [
     {
-      icon: "",
+      icon: EuroSvg01,
       title: "Everything you need and no hidden costs.",
       description:
         "There are lots of factors that affect the overall price of video production and photography shoots, such as props, backgrounds, models and locations. This is why there's no one-size-fits all price, and most creative agencies won't put any prices on their websites - as they can be misleading without a lot of description.",
     },
     {
-      icon: "",
+      icon: EuroSvg02,
       title: "We'll get you from brief to quote in a flash!",
       description:
         "We will work with you to understand your brief and the full photography and video production requirements of your project, before delivering a budget breakdown to you at the beginning of the process. This gives you a fully honest and transparent view of the resources that make up your project. There's always room for conversation and tweaks, and we're also happy to start with your ideal spend and work backwards.",
@@ -60,4 +62,85 @@ export const contactFormData = {
     "Drop us a mail, let us know your budget, and we'll share what we can offer",
   fields: baseContactFormFields,
   submitLabel: "Send",
+};
+
+export const serviceDetailsSliderData = {
+  slides: [
+    {
+      headline: "Video Production…",
+      image: {
+        src: "",
+        alt: "",
+      },
+      paragraphs: [
+        {
+          text: "Video production is a more complex process compared with photography, both in planning, production and the fact that the edit time will span days rather than hours, as it does it photography.",
+          highlightedTexts: ["Video production"],
+        },
+        {
+          text: "Similarly to a photo shoot, you'll need to think about the location: from studio spaces to Airbnbs for lifestyle work, and the consideration for sound as well.",
+          highlightedTexts: [],
+        },
+        {
+          text: "We may need a bigger team, if lighting, sound, or set requirements are needed. Additional equipment such as autocues, specialist lighting and microphones, cameras dedicated to slow motion may also be considered for more creative briefs.",
+          highlightedTexts: [],
+        },
+        {
+          text: "Actors and presenters, hair and makeup, set elements, props and materials, even fashion stylists, food stylists may required too.",
+          highlightedTexts: [],
+        },
+        {
+          text: "Editing often needs multiple days to complete, and we'll best estimate this for you, allowing for time to feed back and make changes. We can also include music, sound effects and voiceover.",
+          highlightedTexts: [],
+        },
+        {
+          text: "The Pocket Creatives team will always provide a full breakdown when we quote for you, which then makes it easier to see what resources your project needs to get made.",
+          highlightedTexts: ["Pocket Creatives"],
+        },
+        {
+          text: "To give you a general idea, a full day of filming with us starts at £940+VAT.",
+          highlightedTexts: ["£940+VAT"],
+        },
+      ],
+      nextLabel: "Photography",
+    },
+    {
+      headline: "Photography…",
+      image: {
+        src: "",
+        alt: "",
+      },
+      paragraphs: [
+        {
+          text: "Video production is a more complex process compared with photography, both in planning, production and the fact that the edit time will span days rather than hours, as it does it photography.",
+          highlightedTexts: ["Video production"],
+        },
+        {
+          text: "Similarly to a photo shoot, you'll need to think about the location: from studio spaces to Airbnbs for lifestyle work, and the consideration for sound as well.",
+          highlightedTexts: [],
+        },
+        {
+          text: "We may need a bigger team, if lighting, sound, or set requirements are needed. Additional equipment such as autocues, specialist lighting and microphones, cameras dedicated to slow motion may also be considered for more creative briefs.",
+          highlightedTexts: [],
+        },
+        {
+          text: "Actors and presenters, hair and makeup, set elements, props and materials, even fashion stylists, food stylists may required too.",
+          highlightedTexts: [],
+        },
+        {
+          text: "Editing often needs multiple days to complete, and we'll best estimate this for you, allowing for time to feed back and make changes. We can also include music, sound effects and voiceover.",
+          highlightedTexts: [],
+        },
+        {
+          text: "The Pocket Creatives team will always provide a full breakdown when we quote for you, which then makes it easier to see what resources your project needs to get made.",
+          highlightedTexts: ["Pocket Creatives"],
+        },
+        {
+          text: "To give you a general idea, a full day of filming with us starts at £940+VAT.",
+          highlightedTexts: ["£940+VAT"],
+        },
+      ],
+      nextLabel: "Video Production",
+    },
+  ],
 };
